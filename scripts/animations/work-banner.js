cb6a35cedc4a11645cfcf05dcc8eb83aae27b1a4
Prabh <prabhcms@gmail.com>
2024-12-30 20:18:19 -0330

    Work Banner Created

---
+++ scripts/animations/work-banner.js
@@ -0,0 +1,15 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const banner = document.querySelector('.banner-content');
+
+gsap.to(banner, {
+    x: () => -(banner.offsetWidth - window.innerWidth) * 0.5, // Increased movement range
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".work-banner",
+        start: "top 40%", // Changed to start when banner hits center
+        end: "+=180%",      // Increased scroll distance
+        scrub: 2,           // Made scrub faster for smoother movement
+       markers: true    // Uncomment to see trigger points
+    }
+});
